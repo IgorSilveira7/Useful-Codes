1727752a9a3357fbc21c6da2a4132cbfad0a2090
pereiraIgor <igor.pereira@ccc.ufcg.edu.br>
2020-10-16 12:16:28 -0300

    add RadixSort

--- src/sorting/algorithms/Codes.java
+++ src/sorting/algorithms/Codes.java
@@ -322,4 +322,26 @@ public class Codes {
 		array[index1] = array[index2];
 		array[index2] = aux;
 	}
+
+
+	// Radix Sort
+	protected static int d = 8;
+	protected static int w = 32;
+	public static int[] radixSort(int[] a) {
+		int[] b = null;
+		for (int p = 0; p < w/d; p++) {
+			int c[] = new int[1<<d];
+			b = new int[a.length];
+			for (int i = 0; i < a.length; i++)
+				c[(a[i] >> d*p)&((1<<d)-1)]++;
+			for (int i = 1; i < 1<<d; i++)
+				c[i] += c[i-1];
+			for (int i = a.length-1; i >= 0; i--)
+				b[--c[(a[i] >> d*p)&((1<<d)-1)]] = a[i];
+			a = b;
+		}
+		return b;
+	}
+
+
 }
